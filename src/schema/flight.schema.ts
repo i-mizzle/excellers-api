c2f666da5f4ecaa74b50f7d4e98fd88678d6d86d
Immanuel Onum <immanuel.o.onum@gmail.com>
2023-05-10 09:07:40 +0100

    validate number of adults vs infants on a flight search

--- src/schema/flight.schema.ts
+++ src/schema/flight.schema.ts
@@ -4,11 +4,20 @@ export const searchFlightSchema = object({
     body: object({
         adults: number().required('number of adults is required'),
         children: number().required('number of children is required'),
-        infants: number().required('number of infants is required'),
+        infants: number().required('number of infants is required').test(
+            'check-less-than',
+            'number of infants must be less than or equal to number of adults',
+            function (value) {
+              const { adults } = this.parent;
+              if (value !== undefined) {
+                return value <= adults;
+              }
+              return true;
+            }),
         cabin: string().required('cabin is required'),
         departureDate: string().required('departureDate is required'),      
         destination: string().required('destination airport code is required'),      
-        origin: string().required('origin airport code is required'),      
+        origin: string().required('origin airport code is required')  
     })
 });
 
