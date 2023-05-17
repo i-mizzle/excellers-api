f7a0815abf8f2e4ec5ce03a83b22b8dace984098
Immanuel Onum <immanuel.o.onum@gmail.com>
2023-05-17 15:39:39 +0100

    increase size limits

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,10 @@ const host = config.get('host') as string;
 const app = express();
 app.use(enableCors);
 app.use(deserializeUser)
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }))
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 app.listen(port, host, () => {
     log.info(`server is listening at http://${host}:${port}`);
