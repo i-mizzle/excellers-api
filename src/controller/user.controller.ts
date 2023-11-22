8deae04d90072e09e57e1812b16c20aec22ad1c3
Immanuel Onum <immanuel.o.onum@gmail.com>
2023-11-22 11:25:57 +0100

    add user bulk create

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -421,3 +421,47 @@ export async function changePasswordHandler(req: Request, res: Response) {
         return response.error(res, error)
     }
 }
+
+export async function bulkImportUsers(req: Request, res: Response) {
+    try {
+        // const password= req.body.password
+        // const newPassword = req.body.newPassword
+        // const userId = get(req, 'user._id');
+
+        // const user = await findUser({_id: userId}) 
+
+        // if(!user) {
+        //     return response.notFound(res, 'user not found')
+        // }
+        // // const user = await findUser({_id: userId})
+        
+        // const validated = await validatePassword({username: user.username, password});
+        // if (!validated) {
+        //     return response.unAuthorized(res, { message: "invalid username or password" })
+        // }
+
+        // await changePassword(mongoose.Types.ObjectId((user._id)), newPassword)
+        // return response.ok(res, {message: 'Password updated successfully'})
+        let created = 0
+        await Promise.all(req.body.data.map(async (item: any) => {
+            await createUser({
+                email: item.email,
+                username: item.username,
+                name: item.name,
+                phone: item.phone,
+                idNumber:item.idNumber,
+                permissions: item.permissions,
+                password: atob(item.password),
+                passwordChanged: item.document.passwordChanged,
+                userType: 'ADMIN'
+            })
+            created += 1
+        }))
+
+        return response.ok(res, {message: `${created} users created successfully.`}) 
+    } catch (error: any) {
+        log.error(error)
+        return response.error(res, error)
+    }
+}
+
